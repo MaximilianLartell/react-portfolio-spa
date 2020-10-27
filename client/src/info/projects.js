16ace6228d44ee3e802f751fa07757faa764e646
Maximilian Skoglund Lartell <max.lartell@gmail.com>
2020-10-27 10:14:26 +0100

    timeline started

---
+++ client/src/info/projects.js
@@ -0,0 +1,30 @@
+const jobHistory = () => {
+  return [
+    {
+      name: 'Lorem pippsum',
+      description: 'Water and sanitary systems design',
+      url: 'At Sweco I worked with the design of sustainable.',
+      homepage: 'https://www.google.se',
+      created_at: '2020-20-20',
+      language: 'Python',
+    },
+    {
+      name: 'dahoola',
+      description: 'Water and sanitary systems design',
+      url: 'At Sweco I worked with the design of sustainable.',
+      homepage: 'https://www.google.se',
+      created_at: '2020-20-20',
+      language: 'JavaScript',
+    },
+    {
+      name: 'bandoola',
+      description: 'Water and sanitary systems design',
+      url: 'At Sweco I worked with the design of sustainable.',
+      homepage: 'https://www.google.se',
+      created_at: '2020-20-20',
+      language: 'Python',
+    },
+  ];
+};
+
+export default jobHistory;
--- client/src/pages/Projects/ProjectCard.js
+++ client/src/pages/Projects/ProjectCard.js
@@ -1,5 +1,4 @@
-import React from "react";
-import Fade from "react-reveal/Fade";
+import React from 'react';
 
 function ProjectCard({
   name,
@@ -11,31 +10,32 @@ function ProjectCard({
   i,
 }) {
   return (
-    <div className="projects_project-card">
-      <Fade left={i % 2 === 0 ? true : false} right={i % 2 === 0 ? false : true}>
-        <h4 className="project-card_name">{name}</h4>
-        <p className="project-card_description">{description}</p>
-        <p className="project-card_created">{created_at.split("T")[0]}</p>
-        <p className="project-card_language">{language}</p>
-        <div className="project-card_link-container">
+    <div className='projects_project-card'>
+      <div className='project-card-content'>
+        <h4 className='project-card_name'>{name}</h4>
+        <p className='project-card_description'>{description}</p>
+        <p className='project-card_created'>{created_at.split('T')[0]}</p>
+        <p className='project-card_language'>{language}</p>
+        <div className='project-card_link-container'>
           <div
-            className="project-card_link"
-            onClick={() => window.open(url, "_blank")}
+            className='project-card_link'
+            onClick={() => window.open(url, '_blank')}
           >
             Github
           </div>
           {homepage ? (
             <div
-              className="project-card_link"
-              onClick={() => window.open(homepage, "_blank")}
+              className='project-card_link'
+              onClick={() => window.open(homepage, '_blank')}
             >
               Webpage
             </div>
           ) : (
-            ""
+            ''
           )}
         </div>
-      </Fade>
+        <span className='circle' />
+      </div>
     </div>
   );
 }
