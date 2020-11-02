9b3f781d8d525ad990ce602699f754a487024afe
Maximilian Skoglund Lartell <max.lartell@gmail.com>
2020-11-02 18:26:05 +0100

    changed text

--- client/src/info/jobHistory.js
+++ client/src/info/jobHistory.js
@@ -1,24 +1,27 @@
 const jobHistory = () => {
   return [
     {
-      employer: "Sweco",
-      position: "Water and sanitary systems design",
-      description: "At Sweco I worked with the design of sustainable water and sanitary systems in some of Sweden's largest infrastructure projects. When working on fine details in large multidiciplinary projects I learned how clear comunication with the other technologies involved was vital in order to maintain the big picture perspective, and deliver the best product possible.",
-      years: "2018-2020",
+      employer: 'Sweco',
+      position: 'Water and sanitary systems design',
+      description:
+        "At Sweco I worked with the design of sustainable water and sanitary systems in some of Sweden's largest infrastructure projects. When working on fine details in large multidiciplinary projects I learned how clear comunication with the other technologies involved was vital in order to maintain the big picture perspective, and deliver the best product possible. During the first nine months of my employment at Sweco I was also participating in the company's popular trainee program which ran in parallel with my consultant role.",
+      years: '2018-2020',
     },
     {
-      employer: "Swedbank",
-      position: "Customer service",
-      description: "During my studies at KTH I wokred part time as customer service to corporate clients regarding merchant agreements and terminals for payment-services, as well as internal advisor to Swedbanks employees regarding the same matters.",
-      years: "2013-2018",
+      employer: 'Swedbank',
+      position: 'Customer service',
+      description:
+        'During my studies at KTH I wokred part time as customer service to corporate clients regarding merchant agreements and terminals for payment-services, as well as internal advisor to Swedbanks employees regarding the same matters.',
+      years: '2013-2018',
     },
     {
-      employer: "Renee Voltaire",
-      position: "Production/sales",
-      description: "I worked with the production, as well as stand in sales personel, for this fast growing health food company. The rapid expansion the company underwent during my time there made the working environment fast paced. With new challanges arising on a daily basis, a problem solving oriented mindset was essential.",
-      years: "2010-2012",
+      employer: 'Renee Voltaire',
+      position: 'Production/sales',
+      description:
+        'I worked with the production, as well as stand in sales personel, for this fast growing health food company. The rapid expansion the company underwent during my time there made the working environment fast paced. With new challanges arising on a daily basis, a problem solving oriented mindset was essential.',
+      years: '2010-2012',
     },
   ];
-}
+};
 
 export default jobHistory;
